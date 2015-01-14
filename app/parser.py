3ac9107dbff9be764186f0fd1b0c28f82e8642b5
Dan Auerbach <dan@indiegogo.com>
2015-01-13 16:39:11 -0800

    WIP: add upload and audio routes but no templates yet so they aren't working

--- app/parser.py
+++ app/parser.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
+from flaskext.uploads import UploadSet
 app = Flask(__name__)
+
+audios = UploadSet('audios')
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'audio' in request.files:
+        filename = audios.save(request.files['audio'])
+        rec = Audio(filename=filename, user=g.user.id)
+        rec.store()
+        flash("Audio saved.")
+        return redirect(url_for('show', id=rec.id))
+    return render_template('upload.html')
+
+@app.route('/audio/<id>')
+def show(id):
+    audio = Audio.load(id)
+    if audio is None:
+        abort(404)
+    url = audios.url(audio.filename)
+    return render_template('show.html', url=url, audio=audio)
+
 if __name__ == '__main__':
+    app.debug = True
     app.run()
